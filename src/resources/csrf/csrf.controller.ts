c9d8647417ce336e654b504a9864c6939d51c0fe
Thiago Ferreira <thiagofer2003@outlook.com>
2024-11-11 20:45:44 -0300

    hotfix: cors pt. 2

--- src/resources/csrf/csrf.controller.ts
+++ src/resources/csrf/csrf.controller.ts
@@ -1,23 +1,25 @@
 import { Controller, Post, Req, Res } from '@nestjs/common';
 import { doubleCsrf } from 'csrf-csrf';
-import { Response } from 'express';
+import { Request, Response } from 'express';
 
 @Controller('csrf')
 export class CsrfController {
   constructor() {}
 
   @Post()
-  getCsrfToken(@Req() req, @Res() res: Response) {
+  getCsrfToken(@Req() req: Request, @Res() res: Response) {
     const { generateToken } = doubleCsrf({
       getSecret: () => process.env.CSRF_SECRET,
     });
+
     const csrfToken = generateToken(req, res, true, true);
     res.cookie('token', csrfToken, {
-      httpOnly: process.env.NODE_ENV === 'production',
+      httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       signed: true,
-      sameSite: 'strict'
+      sameSite: 'none'
     });
+
     res.json({ csrfToken });
   }
 }
